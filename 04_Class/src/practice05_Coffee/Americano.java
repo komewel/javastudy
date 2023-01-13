e26920f411c5959a29dc2533ae83515ee0d7a9b9
komewel <kimcomewel@gmail.com>
2023-01-14 00:33:58 +0900

    2023-01-14

--- 04_Class/src/practice05_Coffee/Americano.java
+++ 04_Class/src/practice05_Coffee/Americano.java
@@ -8,6 +8,7 @@ public class Americano {
 	private Espresso[] cups;  // 에스프레소 여러 잔 들어갈 수 있음
 	private int shotCount;    // 아메리카노에 들어간 에스프레소 개수(샷)
 	private int extraWater;   // 물
+	private int idx;          // cups 배열의 인덱스
 	
 	// 생성자
 	
