2c5aab888ad37afd6c7319d634921e42191c6ca1
komewel <kimcomewel@gmail.com>
2023-01-26 17:49:27 +0900

    2023-01-26

--- note/src/practice/MainClass.java
+++ note/src/practice/MainClass.java
@@ -4,34 +4,21 @@ import java.io.File;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONPointer;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		
 
 		JSONObject a = new JSONObject();
-		a.put("model", "세탁기");
-		a.put("maker", "삼성");
-		a.put("price", 100);
-		JSONObject b = new JSONObject();
-		b.put("model", "세탁기");
-		b.put("maker", "삼성");
-		b.put("price", 100);
-		JSONObject d = new JSONObject();
-		d.put("model", "세탁기");
-		d.put("maker", "삼성");
-		d.put("price", 100);
+		a.put("지역", "경기도");
+		a.put("수도", "서울");
 		
-		JSONArray c = new JSONArray();
-		c.put(a);
-		c.put(b);
-		c.put(d);
-		
-		System.out.println(c.toString());
-		
-		
-		
+		JSONArray b = new JSONArray();
+		b.put(a);
+		 
+	
+		System.out.println(b.toString());
 		}
 	}
 
